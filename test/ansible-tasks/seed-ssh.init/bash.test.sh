a679116c98f442a00638e04858d600df3ac0d590
xiaoyao9184 <6614349+xiaoyao9184@users.noreply.github.com>
2023-09-08 15:32:33 +0800

    Add test seed-ssh.init

---
+++ test/ansible-tasks/seed-ssh.init/bash.test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+source_directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+inventory_path=$(realpath ${PWD}/../../test@local/2ssh/ansible-inventories)
+echo $inventory_path
+
+ansible-playbook -vvv -i $inventory_path $source_directory/ansible-playbook.init.yml
+
+
+# 0
+ansible-playbook -vvv -e ansible_password=seed -i $inventory_path $source_directory/ansible-playbook.test0.yml
+
+
+# 1
+pkill ssh
+sleep 2
+ansible-playbook -vvv -i $inventory_path --extra-vars "mode=failed" $source_directory/ansible-playbook.ping.yml
+
+ansible-playbook -vvv -e ansible_password=seed -i $inventory_path $source_directory/ansible-playbook.test1.yml
+
+pkill ssh
+sleep 2
+ansible-playbook -vvv -i $inventory_path --extra-vars "mode=succeeded" $source_directory/ansible-playbook.ping.yml
+
+
+ansible-playbook -vvv -i $inventory_path $source_directory/ansible-playbook.clean.yml
