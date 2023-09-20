17b24f499e5fa2f6e4c944c56bccc691b14b4c88
xiaoyao9184 <6614349+xiaoyao9184@users.noreply.github.com>
2023-09-20 20:54:30 +0800

    Update wsl.config bash

--- seed/bash/wsl.config.sh
+++ seed/bash/wsl.config.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
+set -e
+
+[[ -z "$(command -v ansible-playbook)" ]] && source ~/.profile
+
 source_directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 seed_workpath="$(realpath $source_directory/../)"
 
 ansible-playbook -K $seed_workpath/ansible-playbook/seed-wsl/wsl-automount.metadata.yml
 ansible-playbook -K $seed_workpath/ansible-playbook/seed-wsl/wsl-windowspath.disable.yml
+
+echo "NOTE: need reboot wsl"
+
+# sudo touch /var/run/reboot-required
+# sudo killall -r '.*'
+if [[ -n "$(command -v wsl.exe)" ]];
+then
+    echo "terminate wsl $WSL_DISTRO_NAME"
+    wsl.exe --terminate $WSL_DISTRO_NAME
+else
+    echo "You need exit wsl and run 'wsl --shutdown' or 'wsl --terminate $WSL_DISTRO_NAME'"
+fi
