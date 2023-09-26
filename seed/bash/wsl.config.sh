4bfaf4ce339c4032ed760ade4d9675af027f7ead
xiaoyao9184 <6614349+xiaoyao9184@users.noreply.github.com>
2023-09-26 16:43:11 +0800

    fix wsl.config

--- seed/bash/wsl.config.sh
+++ seed/bash/wsl.config.sh
@@ -8,7 +8,6 @@ source_directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pw
 
 seed_workpath="$(realpath $source_directory/../)"
 
-ansible-playbook -K $seed_workpath/ansible-playbook/seed-wsl/wsl-automount.metadata.yml
 ansible-playbook -K $seed_workpath/ansible-playbook/seed-wsl/wsl-windowspath.disable.yml
 
 echo "NOTE: need reboot wsl"
