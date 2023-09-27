3dd1b4a17c4a6ae74d66bd79af221a7110005d89
xiaoyao9184 <6614349+xiaoyao9184@users.noreply.github.com>
2023-09-27 15:31:01 +0800

    Add test docker-volume.push

---
+++ test/ansible-tasks/seed-docker/docker-volume.push/bash.test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+source_directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+inventory_path=$(realpath ${PWD}/../../test@local/4docker/ansible-inventories)
+echo $inventory_path
+
+ansible-playbook -vvv -i $inventory_path $source_directory/ansible-playbook.init.yml
+
+
+# 0
+ansible-playbook -vvv -i $inventory_path $source_directory/ansible-playbook.test0.yml
+
+
+# 1
+ansible-playbook -vvv -i $inventory_path $source_directory/ansible-playbook.test1.yml
+
+
+ansible-playbook -vvv -i $inventory_path $source_directory/ansible-playbook.clean.yml
