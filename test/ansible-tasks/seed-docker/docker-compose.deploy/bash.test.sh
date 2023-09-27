a18d322e0d5b8cbea0ffaacda801794e50e7132e
xiaoyao9184 <6614349+xiaoyao9184@users.noreply.github.com>
2023-09-27 16:24:02 +0800

    Add test docker-compose.deploy

---
+++ test/ansible-tasks/seed-docker/docker-compose.deploy/bash.test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+
+source_directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+inventory_path=$(realpath ${PWD}/../../test@local/4docker/ansible-inventories)
+echo $inventory_path
+
+ansible-playbook -vvv -K -i $inventory_path $source_directory/ansible-playbook.init.yml
+
+
+# 0
+ansible-playbook -vvv -i $inventory_path $source_directory/ansible-playbook.test0.yml
+
+
+# 1
+ansible-playbook -vvv -i $inventory_path $source_directory/ansible-playbook.test1.yml
+
+
+# 2
+ansible-playbook -vvv -i $inventory_path $source_directory/ansible-playbook.test2.yml
+
+
+# 3
+ansible-playbook -vvv -i $inventory_path $source_directory/ansible-playbook.test3.yml
+
+
+# 4
+ansible-playbook -vvv -K -i $inventory_path $source_directory/ansible-playbook.test4.yml
+
+
+ansible-playbook -vvv -K -i $inventory_path $source_directory/ansible-playbook.clean.yml
